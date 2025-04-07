1d7900081759adce5174407c0f21389b767fc62f
asrarfarooqwani <afarooq_be23@thapar.edu>
2025-04-07 14:38:23 +0530

    Create asigment8.py

---
+++ asigment8.py
@@ -0,0 +1,19 @@
+import sklearn as sk
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X = iris.data       
+y = iris.target      
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+sample = [[0.1, 0.5, 0.4, 0.2]]
+prediction = model.predict(sample)
+print("Predicted class:", iris.target_names[prediction[0]])
+
